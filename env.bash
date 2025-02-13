3c2f616d63d0a3b31f568636eeebd57a5aeba510
TenrouHoshina <masiro.to.akari@gmail.com>
2025-02-13 11:24:51 +0800

    APTインストール時のメッセージを追加

--- env.bash
+++ env.bash
@@ -145,6 +145,7 @@ function apt_in() {
     if [[ ${LINE} =~ ^#.* ]]; then continue; fi
 
     export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1
+    echo -e "#####################\n[APT] Install '${LINE}'\n#####################"
     sudo -E apt install -y ${LINE}
     if [ $? -ne 0 ]; then
       echo "[ERROR] Install '${LINE}' is failed" >&2
